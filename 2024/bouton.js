58b507eb01df2160ad4f952b3171a00994e62f92
master-mic <121996092+master-mic@users.noreply.github.com>
2024-05-02 16:19:09 -0400

    Add files via upload

--- 2024/bouton.js
+++ 2024/bouton.js
@@ -1,8 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
 	var menuLinks = document.querySelectorAll('.mobile-menu a');
 		menuLinks.forEach(function(link) {
 			link.addEventListener('click', function() {
 			document.querySelector('.mobile-menu-overlay').style.display = 'none';
 		});
 	});
+});*/
+
+
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Add event listeners to each menu item
+menuItems.forEach(item => {
+  // Add event listener for click event
+  item.addEventListener('click', () => {
+    // Remove 'active' class from all menu items
+    menuItems.forEach(menuItem => {
+      menuItem.classList.remove('active');
+    });
+    // Add 'active' class to the clicked menu item
+    item.classList.add('active');
+  });
+
+  // Add event listener for touchstart event (for mobile devices)
+  item.addEventListener('touchstart', () => {
+    // Simulate click event for touch devices
+    item.click();
+  });
 });
